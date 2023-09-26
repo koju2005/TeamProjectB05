a28cb927a089d06f4864987185d1866f4837be99
M1NO001 <dntjd1306@naver.com>
2023-09-26 12:19:52 +0900

    ItemData, Resource 기본적인 틀 추가 및 자원 아이템 생성

---
+++ Assets/Scriptable Object/Scripts/ItemData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public enum ItemType
+{
+    Resource,
+    Equipable,
+    Consumable
+}
+public enum ConsumableType
+{
+    Hunger,
+    thirsty,
+    Health
+}
+
+[CreateAssetMenu(fileName ="Item",menuName ="New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+}
