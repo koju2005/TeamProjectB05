cc203a401771183add89aa3fe61a17f4a556c5d5
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-09-26 17:27:18 +0900

    [Fix] 아이템 정보 출력

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -12,6 +12,7 @@ public class UIManager : MonoBehaviour
     public GameObject prod;//제작창
     public GameObject equip;//장비창
     public GameObject InvenHud;//E버튼 입력시 생기는 모든 개체
+    public GameObject itemInfo;//아이템정보
     private void Awake()
     {
         instance = this;
@@ -28,6 +29,7 @@ public class UIManager : MonoBehaviour
     public void Inven()
     {
         ShowInven(InvenHud, quickslot);
+        itemInfo.SetActive(false);
     }
     private void ShowUI(GameObject obj)
     {
@@ -37,17 +39,24 @@ public class UIManager : MonoBehaviour
         obj.SetActive(false);
     }
 
+    public void CansleInfo()
+    {
+        itemInfo.SetActive(false);
+    }
     private void ShowInven(GameObject obj, GameObject obj2)
     {
+       
         if (obj.activeSelf == false)
         {
             obj2.SetActive(false);
             obj.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
         }
         else
         {
             obj2.SetActive(true);
             obj.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
 }
