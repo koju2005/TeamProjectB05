833625721a728d7c769ccde8d31d12a1a3d2fed7
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-03 00:30:31 +0900

    [Add] gunshotsound 스크립트 추가/ 거너 총쏘는 모션시 효과음 추가
    -스크립트 추가
    -슈팅사운드 효과음 추가

---
+++ Assets/Script/Monster/GunshotSound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunshotSound : MonoBehaviour
+{
+    public AudioSource gunshotAudioSource; 
+    public AudioClip gunshotSound; 
+
+   
+    public void PlayGunshotSound()
+    {
+        gunshotAudioSource.clip = gunshotSound;
+        gunshotAudioSource.Play();
+    }
+}
