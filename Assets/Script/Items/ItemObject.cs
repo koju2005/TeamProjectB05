f33723b63f5c9bbe1a7e4f4e32f53b845c27cad6
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-10-02 13:17:52 +0900

    [Add]상호작용, 공격모션, 카메라 수정

--- Assets/Script/Items/ItemObject.cs
+++ Assets/Script/Items/ItemObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
@@ -14,4 +14,15 @@ public class ItemObject : MonoBehaviour
     {
         item = data;
     }
+    public string GetInteractPrompt()
+    {
+        return string.Format("Pickup {0}", item.displayName);
+    }
+
+    public void OnInteract()
+    {
+      // Inventory.instance.AddItem(this);  인벤토리의 AddItem의 매개변수를 Itemobject가 아니라 ItemData를 받아올수있게 해야할꺼같습니다
+        Destroy(gameObject);
+    }
 }
+
